cc07334ec5d60cd7104550bdd6e169c7f8aa81c6
Richard Sherriff <sherriff8@gmail.com>
2022-03-17 11:39:57 +0000

    Fixed bug where the right hand button on the stage menu would get destroyed

--- ui/menu_stage_ui.py
+++ ui/menu_stage_ui.py
@@ -8,27 +8,27 @@ class MenuStageUI(UI):
 
         bd = [1, 23, 4, 1]  # Button Dimensions
         button_tiles = tiles[bd[0]:bd[0] + bd[2], bd[1]:bd[1] + bd[3]]
-        self.left_button = Button(x=bd[0], y=bd[1], width=bd[2],height=bd[3], click_action=ChangeMenuStageAction(self.section.engine, -1), tiles=button_tiles)
-        self.elements.append(self.left_button)
+        left_button = Button(x=bd[0], y=bd[1], width=bd[2],height=bd[3], click_action=ChangeMenuStageAction(self.section.engine, -1), tiles=button_tiles)
+        self.elements.append(left_button)
 
         
         bd = [46, 23, 4, 1]  # Button Dimensions
         button_tiles = tiles[bd[0]:bd[0] + bd[2], bd[1]:bd[1] + bd[3]]
-        self.right_button = Button(x=bd[0], y=bd[1], width=bd[2],height=bd[3], click_action=ChangeMenuStageAction(self.section.engine, 1), tiles=button_tiles)
-        self.elements.append(self.right_button)
+        right_button = Button(x=bd[0], y=bd[1], width=bd[2],height=bd[3], click_action=ChangeMenuStageAction(self.section.engine, 1), tiles=button_tiles)
+        self.elements.append(right_button)
 
         self.tiles = tiles
 
 
     def setup_level_buttons(self, start_pos):
-        self.elements = self.elements[:1]
+        self.elements = self.elements[:2]
 
         bd = [start_pos[0], start_pos[1], 34, 1]
         for i in range(0,6):
             button_tiles = self.tiles[bd[0]:bd[0] + bd[2], bd[1]:bd[1] + bd[3]]
-            self.level_button = Button(x=bd[0], y=bd[1], width=bd[2],height=bd[3], click_action=MenuSelectLevelAction(self.section.engine, i), tiles=button_tiles)
-            self.level_button.set_hover_action(MenuHoverLevelAction(self.section.engine, i))
-            self.elements.append(self.level_button)
+            level_button = Button(x=bd[0], y=bd[1], width=bd[2],height=bd[3], click_action=MenuSelectLevelAction(self.section.engine, i), tiles=button_tiles)
+            level_button.set_hover_action(MenuHoverLevelAction(self.section.engine, i))
+            self.elements.append(level_button)
 
             bd[1] += 2
 
