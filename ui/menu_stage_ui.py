bb4a44e9c55e7b5503604090569558049f1741cf
Richard Sherriff <sherriff8@gmail.com>
2022-04-11 16:49:43 +0100

    Fixed bug where stage switch buttons stop working after one use

--- ui/menu_stage_ui.py
+++ ui/menu_stage_ui.py
@@ -6,22 +6,26 @@ class MenuStageUI(UI):
     def __init__(self, section, tiles):
         super().__init__(section)
 
+        self.stage_buttons = []
+
         bd = [1, 23, 4, 1]  # Button Dimensions
         button_tiles = tiles[bd[0]:bd[0] + bd[2], bd[1]:bd[1] + bd[3]]
         left_button = Button(x=bd[0], y=bd[1], width=bd[2],height=bd[3], click_action=ChangeMenuStageAction(self.section.engine, -1), tiles=button_tiles)
-        self.elements.append(left_button)
+        self.stage_buttons.append(left_button)
 
         
         bd = [46, 23, 4, 1]  # Button Dimensions
         button_tiles = tiles[bd[0]:bd[0] + bd[2], bd[1]:bd[1] + bd[3]]
         right_button = Button(x=bd[0], y=bd[1], width=bd[2],height=bd[3], click_action=ChangeMenuStageAction(self.section.engine, 1), tiles=button_tiles)
-        self.elements.append(right_button)
+        self.stage_buttons.append(right_button)
 
         self.tiles = tiles
 
 
     def setup_level_buttons(self, start_pos):
-        self.elements = self.elements[:2]
+        self.elements.clear()
+
+        self.elements += self.stage_buttons
 
         bd = [start_pos[0], start_pos[1], 34, 1]
         for i in range(0,6):
