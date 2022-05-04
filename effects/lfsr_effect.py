d179e8d451dcb90dde3592262a9f1ea18dfc8815
Richard Sherriff <sherriff8@gmail.com>
2022-05-04 12:34:19 +0100

    Fixed bugs in LFSR screen effect.

--- effects/lfsr_effect.py
+++ effects/lfsr_effect.py
@@ -8,6 +8,7 @@ class LFSREffect(Effect):
         self.current_tile = 0
         self.size = width * height
         self.tile = c_uint32(1).value
+        self.feedback = 0x62B    
 
         self.temp_console = Console(width=self.width, height=self.height, order="F")
         self.processed_tiles = []
@@ -16,30 +17,30 @@ class LFSREffect(Effect):
         super().start()
         
     def render(self, console):
-        feedback = 0x62B        
-        feedback_this_step = -(self.tile & c_uint32(1).value) & feedback
-        self.tile = (self.tile >> 1) ^ feedback_this_step
 
-        self.time_alive += self.engine.get_delta_time()
+        for i in range(0, 50): 
+            feedback_this_step = -(self.tile & c_uint32(1).value) & self.feedback
+            self.tile = (self.tile >> 1) ^ feedback_this_step
 
-        tile_width = self.tile % self.height
-        tile_height = (self.tile - tile_width) / self.height
+            tile_width = self.tile % self.width
+            tile_height = int((self.tile - tile_width) / self.width)
 
-        self.processed_tiles.append()
+            tile_tuple = (tile_width, tile_height)
 
-        tile_tuple = (tile_width, tile_height)
-        if tile_tuple not in  self.processed_tiles:
-            self.processed_tiles.append((tile_tuple))
-        else:
-            self.stop()
-
-        print(self.tile)
+            if tile_tuple not in self.processed_tiles:
+                self.processed_tiles.append(tile_tuple)
+            else:
+                self.stop()
 
         tile_console = Console(width=1, height=1, order="F")
-        for x in self.width:
-            for y in self.height:
-                if [x,y] not in self.processed_tiles:
-                    tile_console.tiles_rgb = self.tiles[x,y]
-                    tile_console.blit(self.temp_console, src_x=0, src_y=0, dest_x=x, dest_y=y, width=1, height=1)
+        for x in range(0,self.width):
+            for y in range(0,self.height):
+                if (x,y) not in self.processed_tiles:
+                    tile_console.tiles_rgb[0,0] = self.tiles[x,y]
+                    tile_console.blit(console, src_x=0, src_y=0, dest_x=x, dest_y=y, width=1, height=1)
 
-        self.temp_console.blit(console, src_x=0, src_y=0, dest_x=0, dest_y=0, width=self.width, height=self.height)
+
+
+
+
+        
